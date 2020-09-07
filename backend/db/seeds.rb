075c431a28994940c76481308e6b0b4cc1aa9d24
Riley Slayden <slaydenriley@gmail.com>
2020-09-07 13:05:10 -0600

    Setting up associations and seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "Riley Slayden", email: "slaydenriley@gmail.com", password_digest: "codydog")
+User.create(name: "Morgan Hubbard", email: "mbhubbard@gmail.com", password_digest: "codydog")
